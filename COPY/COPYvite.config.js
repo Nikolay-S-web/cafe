d5744d867f0d53347231c6faa16f0f94a04b7193
Nikolay-S-web <senchenko.n.91@gmail.com>
2025-04-19 18:33:19 +0700

    first commit

---
+++ COPY/COPYvite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
+import autoprefixer from 'autoprefixer';
+import babelPlugin from 'vite-plugin-babel';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+export default defineConfig({
+    base: './',
+    root: path.resolve(__dirname, 'src'),
+    build: {
+        outDir: path.resolve(__dirname, 'dist'),
+        emptyOutDir: true,
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name].[hash][extname]',
+            },
+        },
+    },
+    plugins: [VitePluginSvgSpritemap('./images/icons/*.svg'), babelPlugin(), ViteMinifyPlugin({})],
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({
+                    grid: 'autoplace',
+                }),
+            ],
+        },
+    },
+});
